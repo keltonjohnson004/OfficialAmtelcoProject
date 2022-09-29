07f0d13752654f0ad99f967efccaca21ae35f73b
keltonjohnson004 <kelton.b.johnson@gmail.com>
2022-09-28 22:02:31 -0500

    Initial commit

---
+++ AmtelcoProject/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿namespace AmtelcoProject.Models
+{
+    public class Users
+    {
+
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public DateTime UserCreatedOn { get; set; }
+        public DateTime UserLastLoggedOn { get; set; }
+
+
+    }
+}
